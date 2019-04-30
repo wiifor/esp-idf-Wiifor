ca2147bd59b50b782bff03d7d489662bde3766e2
Ludovic Tancerel <ludovic.tancerel@wiifor.com>
2019-04-30 12:29:34 +0000

    Revert "s217 - Integrated LittleFS library and its examples"
    This reverts commit 0d92d04cbbd05f9b8702fd0d9eebf1474ea21d87.
    
    Change-Id: I3547fb886582b7e4408c8b06c0e596f12e726010

--- components/extflash/component.mk
+++
@@ -1,7 +0,0 @@
-#
-# Component Makefile
-#
-
-COMPONENT_SRCDIRS = components/$(COMPONENT_NAME)
-COMPONENT_ADD_INCLUDEDIRS = components/$(COMPONENT_NAME)/include
-
--- components/littleflash/component.mk
+++
@@ -1,7 +0,0 @@
-#
-# Component Makefile
-#
-
-COMPONENT_SRCDIRS := . littlefs
-COMPONENT_ADD_INCLUDEDIRS := include littlefs
-
